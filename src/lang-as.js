6706e3edbdd35b33c8de994ac7264f18eabacf1c
ProjectSoft <projectsoft2009@yandex.ru>
2014-11-30 06:20:44 +0400

    Update lang-as.js
    Prettify lang ActionScript

--- src/lang-as.js
+++ src/lang-as.js
@@ -7,11 +7,8 @@ PR.registerLangHandler(
 		],
 		[
 			[PR.PR_KEYWORD,/^(?:undefined|false|null|this|true|flash_proxy|object_proxy|default|xml|namespace|import|include|use|break|case|continue|do|while|else|for|in|each|if|label|return|switch|throw|try|catch|finally|with|dynamic|final|internal|native|override|private|protected|public|static|class|const|extends|function|get|implements|interface|package|set|var|as|new|void)\b/],
-			// Classes first character Uppercase
 			[PR.PR_TYPE,/^[A-Z][_$A-Z0-9]*[a-z][\w$]*\b/,null],
-			// Name Events UPPERCASE
 			[PR.PR_TYPE,/^(\.\v)?[A-Z][_$A-Z0-9][\w$]*\b/,null],
-			// Types lower case
 			[PR.PR_TYPE,/^(?:(u)?int)\b/,null],
 			[PR.PR_FUNCTION,/^(?:super|trace|decodeURI|decodeURIComponent|encodeURI|encodeURIComponent|escape|isFinite|isNaN|isXMLName|parseFloat|parseInt|unescape)\b/],
 			[PR.PR_COMMENT,/^\/\/[^\r\n]*/,null],
@@ -19,5 +16,6 @@ PR.registerLangHandler(
 			[PR.PR_PLAIN,/^[a-z_$][a-z_$@0-9]*/i,null],
 			[PR.PR_LITERAL,/^(?:0x[a-f0-9]+|(?:\d(?:\.\d)?|\.\d\+)(?:e[+\-]?\d+)?)[a-z]*/i,null,"0123456789"]
 		]
-	),["ActionScript","as"]
+	),
+	["ActionScript","as"]
 );
